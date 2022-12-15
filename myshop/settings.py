52aa817c8fb5881e9acee6598664e0c5be147cb6
Prateek Lodhi <mailprateeklodhi@gmail.com>
2022-12-15 15:58:21 +0530

    Order Model

--- myshop/settings.py
+++ myshop/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
-    'orders.appas.OrdersConfig',
+    'orders.apps.OrdersConfig',
 ]
 
 MIDDLEWARE = [
--- myshop/urls.py
+++ myshop/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('shop.urls',namespace='shop')),
     path('cart/',include('cart.urls',namespace='cart')),
+    path('order/',include('orders.urls',namespace='orders'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
