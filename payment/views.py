746df0c40126898bc32c68617ee7ddb49758b236
Prateklodhi <mailprateeklodhi@gmail.com>
2022-12-28 17:20:04 +0530

    html commit

--- payment/views.py
+++ payment/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
 from decimal import Decimal
 import stripe
 from django.conf import settings
 from django.shortcuts import render, redirect, reverse,get_object_or_404
 from orders.models import Order
 # Create your views here.
+
 # create the stripe instance
 stripe.api_key = settings.STRIPE_SECRET_KEY
 stripe.api_version = settings.STRIPE_API_VERSION
