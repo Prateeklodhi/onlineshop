9ef0de8910a67fcc8c4b807862dc97a5ab406da4
Prateklodhi <mailprateeklodhi@gmail.com>
2022-12-28 14:58:02 +0530

    payment gatway

--- payment/views.py
+++ payment/views.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.shortcuts import render, redirect, reverse,get_object_or_404
 from orders.models import Order
 # Create your views here.
-#create the stripe instance
+# create the stripe instance
 stripe.api_key = settings.STRIPE_SECRET_KEY
 stripe.api_version = settings.STRIPE_API_VERSION
 
@@ -14,7 +14,7 @@ def payment_process(request):
     order = get_object_or_404(Order,id=order_id)
     if request.method =='POST':
         success_url = request.build_absolute_uri(reverse('payment:completed'))
-        cancel_url = request.build_absolute_uri(reverse('payment:cenceled'))
+        cancel_url = request.build_absolute_uri(reverse('payment:canceled'))
         session_data = {
             'mode':'payment',
             'client_reference_id':order.id,
@@ -37,7 +37,7 @@ def payment_process(request):
         #create stripe checkout session
         session = stripe.checkout.Session.create(**session_data)
         #redirect to stripe payment form
-        return redirect(success_url,code=303)
+        return redirect(session.url, code=303)
     else:
         return render(request,'payment/process.html',locals())
 
